fca9241d442a3954e6e557440a11069069672b3e
xie-qianyue <xie.qianyue@gmail.com>
2015-04-10 22:16:12 +0200

    refine the codes

--- routes/web.js
+++ routes/web.js
@@ -1,10 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function (req, res) {
-    res.sendFile('index.html');
-});
-
 // to solve the F5/Refresh problem
 // remember : it should bt the last call of ther router's GET method, otherwise it will intercept other GET methods
 router.get('/*', function (req, res) {
