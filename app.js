2f6552feb20771e76a1b2c6511df53c1336c535e
XIE Qianyue <xie.qianyue@gmail.com>
2015-10-19 15:45:11 +0200

    correct bug

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser');
 // make it easy to test
 var app = module.exports = express();
 var webRouter = require('./routes/web');
-// var api = require('./routes/api');
+var api = require('./routes/api');
 
 app.set('port', process.env.PORT || 3000);
 
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 // serve the static files
 app.use(express.static('public'));
 // match the api url with a prefix '/api'
-// app.use('/api', api);
+app.use('/api', api);
 app.use('/', webRouter);
 
 // error handling middleware should be loaded after the loading the routes
